b9d0fe56d7b90b9dbd36c4819f70139e874d8cca
logan <logan.cardoso@dgtallab.com.br>
2025-01-15 09:46:12 -0300

    feat: atv 1

---
+++ model.py
@@ -0,0 +1,42 @@
+faq_ctic = [
+    {
+        "pergunta": "O CTIC faz manutenção em impressoras?",
+        "resposta": "Não, apenas realiza instalação e configuração de impressoras."
+    },
+    {
+        "pergunta": "Posso levar meu computador pessoal ao CTIC para manutenção?",
+        "resposta": "Não, os serviços de manutenção são exclusivamente para computadores que pertencem à UFPA. Os computadores que chegam ao CTIC pertencem às unidades acadêmicas e administrativas da Universidade e só são recebidos com autorização dos técnicos alocados no atendimento do SAGITTA."
+    },
+    {
+        "pergunta": "O CTIC faz manutenção em Smartphones e tablets?",
+        "resposta": "Não, apenas disponibiliza um tutorial para configuração da rede sem fio institucional com o nome 'UFPA 2.0 - Institucional'."
+    },
+    {
+        "pergunta": "O CTIC disponibiliza ou configura alguma rede sem fio não homologada pela solução aplicada na UFPA?",
+        "resposta": "Não, as únicas redes homologadas, disponibilizadas e gerenciadas pelo CTIC são 'UFPA 2.0 - Institucional' e 'Eduroam', qualquer outra rede com nome diferente não é mantida e administrada pela equipe do CTIC."
+    },
+    {
+        "pergunta": "O CTIC utiliza rádios wireless convencionais ou soluções caseiras?",
+        "resposta": "Não, os rádios homologados na rede sem fio mantida pelo CTIC são partes de uma solução corporativa implementada na UFPA, pois possuem uma capacidade de gerência, processamento e transmissão de dados maior que rádios convencionais ou caseiros, o que facilita a administração."
+    },
+    {
+        "pergunta": "O CTIC realiza backup de dados dos sites hospedados em seu data center?",
+        "resposta": "Não, por motivos de espaço em seu storage de armazenamento, apenas os serviços críticos da Universidade (E-mail, SIG-UFPA, Portal, etc.) possuem uma rotina de backups, o backup dos arquivos dos sites dos usuários é de responsabilidade do mesmo conforme está descrito no termo de compromisso assinado no momento da solicitação de hospedagem do site."
+    },
+    {
+        "pergunta": "O CTIC envia E-mail solicitação atualização cadastral, dados pessoais e dados acadêmicos?",
+        "resposta": "Não, nenhuma coordenadoria envia e-mail para os usuários solicitando tais informações, caso o usuário precise alterar algum dado e não conseguir via sistema deve abrir uma solicitação no serviço de atendimento do CTIC, Sagitta, e aguardar o atendimento."
+    },
+    {
+        "pergunta": "O CTIC realiza algum bloqueio de páginas de internet?",
+        "resposta": "Não, no momento nenhuma página de conteúdo web é bloqueada pelo CTIC."
+    },
+    {
+        "pergunta": "O CTIC monitora e registra os acessos de usuários e dispositivos de todo o tráfego de dados na rede da UFPA?",
+        "resposta": "Sim, as equipes de Data Center e Redes monitoram todo o tráfego de dados no ambiente computacional da UFPA, todo o processo de monitoramento está de acordo com a Lei Nº 12.965 intitulada 'Marco Civil da Internet', que determina responsabilidades ao CTIC como provedor de acesso à internet da Comunidade Universitária."
+    },
+    {
+        "pergunta": "O CTIC realiza serviços de cabeamento estruturado e óptico nos prédios da UFPA?",
+        "resposta": "Não, a equipe técnica do CTIC apenas fiscaliza obras de cabeamento estruturado e instalação de fibra óptica, o lançamento de cabos e conectorização é realizado por empresa especializada com profissionais treinados para a tarefa."
+    }
+]
